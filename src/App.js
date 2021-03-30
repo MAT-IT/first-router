f5f451637ddd20a4cfcd565fd2e36fec8bb08e65
matzhd <77358430+matzhd@users.noreply.github.com>
2021-03-30 09:07:12 +0100

    added Nav bar , link and exact attribude

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter , Route} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -10,10 +10,24 @@ function App() {
   return (
     <div >
       <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li >
+              <Link to="/Contact">Contact</Link>
+            </li>
 
-        <Route path='/' component={Home}/>
-        <Route path='/About' component={About}/>
-        <Route path='/Contact' component={Contact}/>
+          </ul>
+        </nav>
+        <hr />
+        <Route exact path='/' component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Contact' component={Contact} />
 
 
       </BrowserRouter>
